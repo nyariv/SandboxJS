d3f2f9e85abda219bae07c37e03ac8986f9aaf87
Nir Yariv <nyariv@gmail.com>
2020-10-30 20:21:44 +0200

    rollup fixes

--- .babelrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: ['@babel/plugin-proposal-optional-chaining'],
-  presets: [
-    ['@babel/preset-env', {modules: 'auto', targets: {"esmodules": true, node: 'current'}}],
-    ['@babel/preset-typescript'],
-  ],
-};
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
@@ -10,14 +9,12 @@ export default {
   input: ['src/Sandbox.ts'],
   output: [
     { file: "dist/Sandbox.js", sourcemap: true, exports: 'named', format: "cjs" },
-    { file: "dist/Sandbox.min.js", sourcemap: true, exports: 'named', format: "cjs", plugins: [terser()] },
+    { file: "dist/Sandbox.min.js", sourcemap: true, exports: 'named', format: "esm", plugins: [terser({keep_fnames: /SandboxFunction/})] },
     { file: "dist/Sandbox.esm.js", sourcemap: true, exports: 'named', format: "esm" },
   ],
   plugins: [
     typescript(),
     resolve({ extensions }),
-    babel({ extensions, babelHelpers: 'bundled' }),
-    filesize(),
-    terser({keep_fnames: /SandboxFunction/})
+    filesize()
   ]
 }
