468c510bad2dd0d7e8bcce09ea52c8faf16da661
Nir <nyariv@gmail.com>
2026-01-30 15:49:01 +0200

    cleaner code

--- src/executor.ts
+++ src/executor.ts
@@ -366,7 +366,7 @@ addOps(LispType.Prop, (exec, done, ticks, a, b: string, obj, context, scope) => 
 
   if (prototypeAccess) {
     if (isFunction) {
-      if (!['name', 'length', 'constructor'].includes(b) && (a.hasOwnProperty(b) || b === '__proto__')) {
+      if (!['name', 'length', 'constructor'].includes(b) && a.hasOwnProperty(b)) {
         const whitelist = context.ctx.prototypeWhitelist.get(a.prototype);
         const replace = context.ctx.options.prototypeReplacements.get(a);
         if (replace) {
